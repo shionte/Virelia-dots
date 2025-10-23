24bd664c00a8073a01c8c206819ae4ea02322125
Shio <shionte98@gmail.com>
2025-10-23 14:26:50 -0700

    Not  ready

--- .config/hypr/scripts/wallpaper/random.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-WALLPAPERS_DIR="$HOME/.config/wallpaper"
-NEW_WALL=$(find $WALLPAPERS_DIR -type f | shuf -n 1)
-
-echo $NEW_WALL
--- .config/hypr/scripts/wallpaper/set-random.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-choose_random="$HOME/.config/hypr/scripts/wallpaper/random.sh"
-$HOME/.config/hypr/scripts/wallpapers/set.sh $($choose_random)
